9f82074a425f298ed8ad732c92e47c0530bf86c5
Mayank Pillai <mayankpillai99@gmail.com>
2025-11-03 18:51:45 +0530

    fix(cors): Update allowed origins for socket and app connections

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://localhost:3000", "https://your-bookconnect-app.vercel.app" ],
     credentials: true,
   })
 );
