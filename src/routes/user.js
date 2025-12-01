f13a183b81f495005d1282a2d7ac70912b0d783c
Mayank Pillai <mayankpillai99@gmail.com>
2025-12-01 19:26:50 +0530

    fix: Filter connection requests with missing `fromUserId` or `toUserId` to ensure data integrity.

--- src/routes/user.js
+++ src/routes/user.js
@@ -18,9 +18,11 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     }).populate("fromUserId", USER_SAFE_DATA);
     // }).populate("fromUserId", ["firstName", "lastName"]);
 
+    const data = connectionRequests.filter((req) => req.fromUserId);
+
     res.json({
       message: "Data fetched successfully",
-      data: connectionRequests,
+      data: data,
     });
   } catch (err) {
     res.status(400).send({ message: err.message });
@@ -40,12 +42,19 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("fromUserId", USER_SAFE_DATA)
       .populate("toUserId", USER_SAFE_DATA);
 
-    const data = connectionRequests.map((row) => {
-      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
-        return row.toUserId;
-      }
-      return row.fromUserId;
-    });
+    const data = connectionRequests
+      .map((row) => {
+        // Handle case where a user was deleted but the connection request still exists
+        if (!row.fromUserId || !row.toUserId) {
+          return null;
+        }
+
+        if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+          return row.toUserId;
+        }
+        return row.fromUserId;
+      })
+      .filter((row) => row !== null);
 
     res.json({ data });
   } catch (err) {
